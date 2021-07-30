964b93c82fc6d127a846a728dc6769815e649ffa
GhaSilva <ghabriel-silva@outlook.com>
2021-07-29 21:16:12 -0300

    Criando tabela cargo

---
+++ src/main/java/br/com/ghabriel/spring/data/orm/Cargo.java
@@ -0,0 +1,24 @@
+package br.com.ghabriel.spring.data.orm;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cargos")
+public class Cargo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String descricao;
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
